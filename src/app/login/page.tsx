26a48c3231abf425cffe77edcc525a5f4b43f9d9
Yggdrae <victorcnicacio@hotmail.com>
2025-04-15 01:05:05 -0300

    refactor: responsividade na tela de login e ajustados tamanhos de fonte no login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,23 +1,22 @@
 'use client'
-import Button from "@/components/Button";
 import Card from "@/components/Card";
 import { HStack } from "@/components/HStack";
-import { InputText } from "@/components/InputText";
 import { LoginForm } from "@/components/LoginForm";
-import { Text } from "@/components/Text";
 import { Layout } from "@/components/ui/Layout";
-import { VStack } from "@/components/VStack";
 import Image from "next/image";
 
-export default function Home() {
-  const buttonType = "primary"
+export default function Login() {
 
   return (
     <Layout>
       <Card className="w-full sm:min-h-[300px] h-[500px]">
-        <HStack className="w-full h-full">
-          <Image className="flex-[2]" src={require("@/assets/conference-room.png")} alt={"Login Image"} />
-          <LoginForm />
+        <HStack className="w-full h-full flex-col lg:flex-row">
+          <Image
+            className="hidden lg:block w-2/3 h-full object-cover"
+            src={require("@/assets/conference-room.png")}
+            alt="Login Image"
+          />
+          <LoginForm className="w-full lg:w-1/3" />
         </HStack>
       </Card>
     </Layout>
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -4,18 +4,18 @@ import { InputText } from "./InputText"
 import { Text } from "./Text"
 import { VStack } from "./VStack"
 
-export const LoginForm = () => {
+export const LoginForm = ({ className = "" }: { className?: string }) => {
     return (
-        <VStack className="flex-[1] bg-[#2A2A2A] p-8">
-            <Text className="text-center text-[32px] sm:text-[24px] text-content-primary font-family-heading font-bold">Acessar Conta</Text>
-            <Text className="text-center text-[18px] sm:text-[12px] text-content-ternary font-family-heading font-bold">Entre com suas credenciais para continuar</Text>
+        <VStack className={`bg-[#2A2A2A] p-6 sm:p-8 flex-grow ${className}`}>
+            <Text className="text-center text-[20px] lg:text-[32px] text-content-primary font-family-heading font-bold">Acessar Conta</Text>
+            <Text className="text-center text-[12px] lg:text-[18px] text-content-ternary font-family-heading font-bold">Entre com suas credenciais para continuar</Text>
             <InputText id="email" label="Email" placeholder="Digite seu email" className="mt-8" />
             <InputText id="password" type="password" label="Senha" placeholder="Digite sua senha" className="mt-8" />
-            <Text className="mt-2 text-end text-[14px] sm:text-[10px] text-content-primary font-family-heading cursor-pointer">Esqueceu sua senha?</Text>
+            <Text className="mt-2 text-end text-[12px] lg:text-[16px] text-content-primary font-family-heading cursor-pointer">Esqueceu sua senha?</Text>
             <Button title={"Acessar"} className="mt-8" />
             <HStack className="justify-center mt-8" gap={1}>
-                <Text className="text-center text-[20px] sm:text-[18px] text-content-ternary font-family-heading">Não tem uma conta?</Text>
-                <Text className="text-center text-[20px] sm:text-[18px] text-content-primary font-family-heading cursor-pointer">Cadastre-se</Text>
+                <Text className="text-center text-[16px] lg:text-[20px] text-content-ternary font-family-heading">Não tem uma conta?</Text>
+                <Text className="text-center text-[16px] lg:text-[20px] text-content-primary font-family-heading cursor-pointer">Cadastre-se</Text>
             </HStack>
         </VStack>
     )
