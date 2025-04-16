e4a271842ce2e6f49393f73744153a27759c72c3
Yggdrae <victorcnicacio@hotmail.com>
2025-04-16 01:13:42 -0300

    fix: mudança no hover do botão

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -55,10 +55,10 @@ export function Navbar({ children, ...props }: NavbarProps) {
               ref={menuRef}
               className="absolute right-0 mt-4 w-48 bg-[#2A2A2A] border border-content-primary/30 rounded-md shadow-lg py-2 z-50"
             >
-              {isLoggedIn ? (
+              {!isLoggedIn ? (
                 <>
                   <Link
-                    href="/profile"
+                    href="/perfil"
                     className="block px-4 py-2 text-sm text-content-primary hover:bg-[#E5D3B3]/10"
                     onClick={() => setShowUserMenu(false)}
                   >
