bc45e559531815f469420056f7db26cbd808eb81
Yggdrae <victorcnicacio@hotmail.com>
2025-04-23 23:54:03 -0300

    feat: componente inputText aceita errorText

--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Text } from "./Text";
+import { HStack } from "./HStack";
 
 interface InputTextProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
   id: string;
   placeholder?: string;
+  errorText?: string;
   className?: string;
 }
 
@@ -13,6 +16,7 @@ export const InputText = ({
   id,
   type = "text",
   placeholder = "",
+  errorText,
   className = "",
   ...props
 }: InputTextProps) => {
@@ -22,7 +26,12 @@ export const InputText = ({
   return (
     <div className={`flex flex-col gap-1 ${className}`}>
       <label htmlFor={id} className="text-sm text-content-primary">
-        {label}
+        <HStack className="gap-2">
+          {label}
+          {errorText ? (
+            <Text className="text-red-500 text-sm">{errorText}</Text>
+          ) : null}
+        </HStack>
       </label>
       <div className="relative w-full">
         <input
@@ -30,7 +39,7 @@ export const InputText = ({
           name={id}
           type={isPassword && !showPassword ? "password" : "text"}
           placeholder={placeholder}
-          className="bg-[#1E1E1E] border border-[#44403c] text-sm text-white placeholder-gray-400 rounded-lg px-4 py-2 pr-10 focus:outline-none focus:ring-2 focus:ring-[#E5D3B3] focus:border-[#E5D3B3] transition-colors w-full"
+          className={`bg-[#1E1E1E] border ${errorText ? "border-red-500" : "border-[#44403c]"} text-sm text-white placeholder-gray-400 rounded-lg px-4 py-2 pr-10 focus:outline-none focus:ring-2 focus:ring-[#E5D3B3] focus:border-[#E5D3B3] transition-colors w-full`}
           {...props}
         />
         {isPassword && (
