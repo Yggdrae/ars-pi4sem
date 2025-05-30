f66b4a0a375b1ba0ad709fd8969257668011c893
Victor Nicacio <victorcnicacio@hotmail.com>
2025-05-30 00:29:53 -0300

    feat: horarios modal

---
+++ src/components/RoomHorariosModal.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { Modal } from "@/components/Modal";
+import { VStack } from "@/components/VStack";
+import { HorarioAccordion } from "./HorarioAccordion";
+import { HStack } from "@/components/HStack";
+import Button from "@/components/Button";
+import { IHorarioPayload } from "@/interfaces/IHorario";
+import { useHorarios } from "@/hooks/useHorarios";
+import { useSalas } from "@/hooks/useSalas";
+
+interface RoomEditModalProps {
+  salaId: number;
+  onClose: () => void;
+}
+
+export default function RoomHorariosModal({
+  salaId,
+  onClose,
+}: RoomEditModalProps) {
+  const { getSalaFullById } = useSalas();
+  const { updateHorariosSala } = useHorarios();
+  const [horariosPorDia, setHorariosPorDia] = useState<
+    Record<number, { inicio: string; fim: string }>
+  >({
+    1: { inicio: "08:00", fim: "14:00" }, // Domingo
+    2: { inicio: "08:00", fim: "18:00" }, // Segunda-feira
+    3: { inicio: "08:00", fim: "18:00" }, // Terça-feira
+    4: { inicio: "08:00", fim: "18:00" }, // Quarta-feira
+    5: { inicio: "08:00", fim: "18:00" }, // Quinta-feira
+    6: { inicio: "08:00", fim: "18:00" }, // Sexta-feira
+    7: { inicio: "08:00", fim: "14:00" }, // Sábado
+  });
+
+  const handleChangeHorario = (
+    dia: number,
+    campo: "inicio" | "fim",
+    valor: string
+  ) => {
+    setHorariosPorDia((prev) => ({
+      ...prev,
+      [dia]: { ...prev[dia], [campo]: valor },
+    }));
+  };
+
+  const handleSalvar = async () => {
+    const payload: IHorarioPayload[] = Object.entries(horariosPorDia).map(
+      ([dia, horario]) => ({
+        salaId,
+        diaDaSemana: Number(dia),
+        horarioInicio: horario.inicio,
+        horarioFim: horario.fim,
+      })
+    );
+
+    await updateHorariosSala(payload);
+    onClose();
+  };
+
+  return (
+    <Modal isOpen={!!salaId} onClose={onClose} title="Editar Horários">
+      <VStack className="gap-10">
+        <HorarioAccordion
+          horarios={horariosPorDia}
+          onChange={handleChangeHorario}
+        />
+        <Button title="Salvar" onClick={handleSalvar} />
+      </VStack>
+    </Modal>
+  );
+}
